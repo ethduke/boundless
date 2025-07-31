1fd90af52705c397d24dc95e947dad0f9c0cb3ea
ethduke <ethduke@proton.me>
2025-07-31 12:56:53 -0500

    fix: conflicting From<anyhow::Error> implementation in OrderMonitorErr

--- crates/broker/src/order_monitor.rs
+++ crates/broker/src/order_monitor.rs
@@ -89,12 +89,6 @@ impl From<DbError> for OrderMonitorErr {
     }
 }
 
-impl From<anyhow::Error> for OrderMonitorErr {
-    fn from(err: anyhow::Error) -> Self {
-        OrderMonitorErr::UnexpectedError(err)
-    }
-}
-
 /// Represents the capacity for proving orders that we have available given our config.
 /// Also manages vending out capacity for proving, preventing too many proofs from being
 /// kicked off in each iteration.
